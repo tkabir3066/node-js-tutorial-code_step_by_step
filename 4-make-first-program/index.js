39bdc5a423a722f6362183f6f1e72e67be43c5bf
Tutul Kabir <tutulkabir@gmail.com>
2023-07-26 17:35:21 +0530

    changes in lecture 4 and 6

--- 4-make-first-program/index.js
+++ 4-make-first-program/index.js
@@ -7,3 +7,15 @@ console.warn(a + b + c);
 // ===Interviews question===//
 
 // what is the difference between node:console and javascript console
+
+/* 
+Node.js is a runtime environment that allows you to execute JavaScript code outside of a web browser. In Node.js, the "console" object represents the standard output device for printing information. It provides methods like "log", "error", and "warn" to display messages in the terminal or command prompt.
+
+
+On the other hand, the JavaScript console refers to the console object available in web browsers. It is a developer tool that allows you to interact with and debug JavaScript code running in a web page. You can access it by opening the browser's developer tools and navigating to the "Console" tab.
+
+While both the Node.js console and the JavaScript console are used for printing messages and debugging, they have
+
+*/
+
+// node js is not a language, is a run time environment which execute code of javascript
--- 6-core module/index.js
+++ 6-core module/index.js
@@ -20,3 +20,12 @@ console.log(__filename);
 // interview question-1
 const gs = require("fs").writeFileSync;
 gs("hola.txt", "hello ,sexy");
+
+/* 
+
+In Node.js, a global module refers to a module that is available globally across all Node.js applications. By default, modules in Node.js are local to the application in which they are installed, meaning they can only be accessed within that specific application's context.
+
+However, there are certain modules that can be installed globally using the -g flag with the npm install command. These global modules are installed in a location specified by the Node.js installation, and can be accessed by any Node.js application on that machine.
+
+Global modules are typically used for utilities or tools that are meant to be used across multiple projects or applications. Examples of popular global modules include frameworks like Express, testing libraries like M
+*/
