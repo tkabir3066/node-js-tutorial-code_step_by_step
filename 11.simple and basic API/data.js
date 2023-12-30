10a1ad88536004c595b7dd2257f22bc06f4ed0ad
Tutul Kabir <tutulkabir@gmail.com>
2023-12-30 21:39:46 +0530

    simple and basic api

---
+++ 11.simple and basic API/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "Tutul Kabir", email: "tutul@gmail.com" },
+  { name: "Shehan Kabir", email: "shehan@gmail.com" },
+  { name: "Ayan Ali", email: "ayan@gmail.com" },
+  { name: "Babu Sk", email: "babu@gmail.com" },
+];
+exports.module = data;
