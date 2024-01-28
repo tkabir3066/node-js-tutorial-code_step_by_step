2a5f2a2d28a05fae8610e3abd3be3c8966fd0b2d
Tutul Kabir <tutulkabir@gmail.com>
2024-01-28 22:00:44 +0530

    events and event emitter in nodejs

---
+++ 46.Events and Events emitter in nodejs/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const EventEmitter = require("events");
+
+const app = express();
+
+const event = new EventEmitter();
+
+let count = 0;
+event.on("countAPI", () => {
+  count++;
+  console.log(count);
+});
+app.get("/", (req, res) => {
+  res.send("api called");
+  event.emit("countAPI");
+});
+app.get("/search", (req, res) => {
+  res.send("search api called");
+  event.emit("countAPI");
+});
+app.get("/update", (req, res) => {
+  res.send("update api called");
+  event.emit("countAPI");
+});
+app.listen(5000);
