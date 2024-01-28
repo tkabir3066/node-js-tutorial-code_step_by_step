2b61796f94f191ebff117c5d589f655620c2e3fd
Tutul Kabir <tutulkabir@gmail.com>
2024-01-28 20:46:34 +0530

    os module in node js

---
+++ 45.OS Module in node js/index.js
@@ -0,0 +1,19 @@
+const os = require("os");
+// console.log(os.arch());
+// console.log(os.freemem());
+// console.log(os.freemem() / (1024 * 1024 * 1024)); //converting bytes in GB
+// console.log(os.totalmem() / (1024 * 1024 * 1024)); //total memory in GB
+
+// console.log(os.hostname()); //DESKTOP-2TBETV8
+// console.log(os.homedir()); //C:\Users\Tutul
+
+// console.log(os.platform()); //win32
+
+console.log(os.userInfo());
+// {
+//     uid: -1,
+//     gid: -1,
+//     username: 'Tutul',
+//     homedir: 'C:\\Users\\Tutul',
+//     shell: null
+//   }
